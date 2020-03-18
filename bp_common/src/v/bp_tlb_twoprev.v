091de2696270ef0b81347838221be1112b2fc55c
Gorkem Caylak <gc48@linux13.ece.uw.edu>
2020-03-18 11:54:39 -0700

    Previous two successful pairs(bp_tlb_twoprev.v) added

--- bp_common/src/v/bp_tlb_twoprev.v
+++ bp_common/src/v/bp_tlb_twoprev.v
@@ -28,14 +28,15 @@ module bp_tlb
 
 `declare_bp_fe_be_if(vaddr_width_p, paddr_width_p, asid_width_p, branch_metadata_fwd_width_p);
 bp_pte_entry_leaf_s r_entry, w_entry, passthrough_entry;
-bp_pte_entry_leaf_s prev_r_entry, r_entry_o;
+bp_pte_entry_leaf_s prev_r_entry, prev2_r_entry, r_entry_o;
 
 logic [lg_els_lp-1:0] cam_w_addr, cam_r_addr, ram_addr;
 logic                 r_v, w_v, cam_r_v;
-logic 		                        bypass_en;
-logic [vtag_width_p-1:0]          prev_vtag;
+logic 		                        bypass_en, bypass2_en;
+logic [vtag_width_p-1:0]          prev_vtag, prev2_vtag;
 logic                             new_read;
-
+logic [entry_width_lp-1:0]        dummy_r_entry_o;
+logic                             correct_bypass;
 assign entry_o    = translation_en_i ? r_entry_o : passthrough_entry;
 assign w_entry    = entry_i;
   
@@ -55,21 +56,24 @@ bsg_dff_reset #(.width_p(1))
   );
 
 always_ff @(posedge new_read) begin
-  if(translation_en_i & ~bypass_en) begin
+  if(translation_en_i & (prev_vtag != vtag_i)) begin
+    prev2_r_entry <= prev_r_entry;
+    prev2_vtag    <= prev_vtag;
     prev_r_entry <=  r_entry;
     prev_vtag    <=   vtag_i;
   end
 end
 
+// always_ff @(posedge v_o) begin
+  
+//   correct_bypass <= ((bypass_en | bypass2_en) & (dummy_r_entry_o == r_entry_o));
+// end
+
 assign bypass_en = ((prev_vtag == vtag_i) & translation_en_i);
+assign bypass2_en = ((prev2_vtag == vtag_i) & translation_en_i);
 
-assign r_entry_o = bypass_en ? prev_r_entry : r_entry;              //1
-//assign r_entry_o = r_entry;                                       //2
-assign v_o = bypass_en ? 1 : (new_read);                            //3
-//assign v_o = new_read;                                            //4
-//assign v_o = bypass_en | (~translation_en_i & r_v);               //5
-
-// make .v : 1 & 4 passes, 2 & 3 fails, 2 & 4 passes, 1 & 3 fails, 1 & 5 fails, 1 & 5_in_dff fails
+assign r_entry_o = bypass_en ? prev_r_entry :  bypass2_en ? prev2_r_entry : r_entry;        
+assign v_o = (bypass_en | bypass2_en) ? 1'b1 : (new_read);
 
 bsg_dff_reset #(.width_p(1))
   miss_v_reg
@@ -107,14 +111,14 @@ bsg_cam_1r1w
   vtag_cam
   (.clk_i(clk_i)
    ,.reset_i(reset_i | flush_i)
-   ,.en_i(1'b1)
+   ,.en_i(1'b1)//~bypass_en)
    
    ,.w_v_i(w_v)
    ,.w_set_not_clear_i(1'b1)
    ,.w_addr_i(cam_w_addr)
    ,.w_data_i(vtag_i)
   
-   ,.r_v_i(r_v)// & ~bypass_en)
+   ,.r_v_i(r_v )//& (~bypass_en & ~bypass2_en)) //modified //this might alter write to entryRam process?
    ,.r_data_i(vtag_i)
    
    ,.r_v_o(cam_r_v)
@@ -133,7 +137,7 @@ bsg_mem_1rw_sync
    ,.reset_i(reset_i)
    ,.data_i(w_entry)
    ,.addr_i(ram_addr)
-   ,.v_i(cam_r_v | w_v)
+   ,.v_i(cam_r_v | w_v) // (cam_r_v & (~bypass_en & ~bypass2_en))  |  w_v) //modif
    ,.w_i(w_v)
    ,.data_o(r_entry)
   );
